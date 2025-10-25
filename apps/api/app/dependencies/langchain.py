df65cc8e428072594625210637e58e0f1302c570
yuran1811 <trieuvanbd123@gmail.com>
2025-10-25 21:57:52 +0700

    feat(api): ✨ finish get user objectives

--- apps/api/app/dependencies/langchain.py
+++ apps/api/app/dependencies/langchain.py
@@ -1,9 +1,13 @@
+from typing import Any
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from models.health_models import HealthInsightsResponse
-from models.langchain_models import ContextData
+from app.models.health_models import HealthInsightsResponse
+
+from .config import GEMINI_API_KEY
+from .supabase import supabase_client
 
 SYSTEM_PROMPT = """You are a health AI assistant. 
 Your goal is to help users achieve their health objectives by analyzing their daily health data and schedule. 
@@ -12,20 +16,29 @@ Based on this information, generate personalized insights and recommendations to
 If necessary, you can also write events to the user's calendar to help them stay on track with their health goals.
 """
 
+
 @tool
-def get_users_objectives():
-  """Fetch the user's health objectives."""
-  pass
+def get_users_objectives(user_id: str):
+    """Fetch the user's health objectives."""
+    try:
+      res = supabase_client.table("users").select("*").eq("id", user_id).execute()
+      if len(res.data) == 0:
+          return "User not found."
+
+      user_data: Any = res.data[0]
+      return f"My step goal is {user_data['step_goal']} steps per day. My custom goals are: {user_data['custom_goals']}"
+    except Exception as e:
+      return f"An error occurred while fetching user objectives: {str(e)}"
 
 @tool
 def get_today_health_data():
   """Fetch the user's health data for today."""
-  pass
+  return "User's health data for today: 8000 steps taken, 7 hours of sleep, 2000 calories consumed."
 
 @tool 
 def get_today_schedule():
   """Fetch the user's schedule for today."""
-  pass
+  return "User's schedule for today: 9 AM - Team Meeting, 11 AM - Project Work, 2 PM - Gym, 5 PM - Dinner with Friends."
 
 @tool
 def write_to_calendar(event_details: str):
@@ -33,17 +46,23 @@ def write_to_calendar(event_details: str):
   pass
 
 llm_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-agent = create_agent(model=llm_model, tools=[get_users_objectives, get_today_health_data, get_today_schedule, write_to_calendar]
-                     , system_prompt=SYSTEM_PROMPT, )
+agent = create_agent(
+  model=llm_model,
+  tools=[get_users_objectives, get_today_health_data, get_today_schedule, write_to_calendar],
+  system_prompt=SYSTEM_PROMPT
+)
 
-
-
-def create_ai_insights(user_id: str) -> HealthInsightsResponse:
+def create_ai_insights(user_id: str) -> HealthInsightsResponse | Any:
   """Generate AI insights based on the user's health data and objectives."""
-  response = agent.invoke({"messages": [{"role": "user", "content": "Generate personalized health insights for me based on their objectives, today's health data, and schedule."}]}, 
-                          context=ContextData(user_id=user_id))
+  response = agent.invoke(
+    {
+      "messages": [
+        {
+          "role": "user",
+          "content": f"Generate personalized health insights for me based on their objectives, today's health data, and schedule. My user ID is {user_id}"
+        }
+      ]
+    },
+  )
 
-  return response
-
-  
-
+  return "\n".join([_["text"] for _ in response["messages"][-1].content if _["type"] == "text"]) or "No insights generated."
---
+++ apps/api/app/dependencies/supabase.py
@@ -0,0 +1,14 @@
+from supabase import Client, create_client
+from supabase.client import ClientOptions
+
+from app.dependencies.config import SUPABASE_KEY, SUPABASE_URL
+
+url: str = SUPABASE_URL or ""
+key: str = SUPABASE_KEY or ""
+supabase_client: Client = create_client(
+    url,
+    key,
+    options=ClientOptions(
+        schema="public",
+    ),
+)
